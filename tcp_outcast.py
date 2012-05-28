0e0d80e5baa86fe178f75a22931bb2092fc872a7
None <None>
2012-05-28 03:35:16 +0000

    Adding functions to FatTreeNodeId to be compatible with Node. Made run_outcast generic to be able to run with FatTreeTopo.

--- tcp_outcast.py
+++ tcp_outcast.py
@@ -7,7 +7,7 @@ import re
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.log import lg, output
-from mininet.node import CPULimitedHost
+from mininet.node import CPULimitedHost, RemoteController
 from mininet.link import TCLink
 from mininet.util import irange, custom, quietRun, dumpNetConnections
 from mininet.cli import CLI
@@ -53,7 +53,7 @@ parser.add_argument('--rto_min',
 
 parser.add_argument('--queue_size',
                     help=('queue size to set on switch.'),
-                    default='16kb')
+                    default='16KB')
 
 parser.add_argument('--time', '-t',
                     dest="time",
@@ -113,8 +113,21 @@ def start_tcpprobe():
 def stop_tcpprobe():
     os.system("killall -9 cat; rmmod tcp_probe &>/dev/null;")
 
-def run_outcast(net, n_h1, n_h2, rto_min, queue_size):
-    "Run experiment"
+def run_outcast(net, receiver, hosts_2hop, hosts_6hop, n_2hops, n_6hops,
+                rto_min, queue_size):
+    """Run outcast experiment.
+
+    Args:
+      receiver: the receiver node.
+      hosts_2hop: a list of host node objects for host nodes
+                  at 2 hops from receiver sending data to receiver.
+      hosts_6hop: a list of host node objects for host nodes
+                  at 6 hops from receiver sending data to receiver.
+      n_2hops: number of flows per 2-hop host.
+      n_6hops: number of flows per 6-hop host.
+      rto_min: RTO min time setting for each host as string.
+      queue_size: Queue size of each switch.
+    """
 
     seconds = args.time
 
@@ -124,41 +137,53 @@ def run_outcast(net, n_h1, n_h2, rto_min, queue_size):
     monitor.start()
     start_tcpprobe()
 
-    # Get receiver and clients
-    recvr = net.getNodeByName('h0')
-    h1 = net.getNodeByName('h1')
-    h2 = net.getNodeByName('h2')
-
     print 'Setting minRTO on each host...'
     # TODO(bhelsley): I think this can be done with os.system anyway.
     cmd = 'ip route replace dev %%s-eth0 rto_min %s' % rto_min
-    print '  h0 ', recvr.cmd(cmd % 'h0')
-    print '  h1 ', h1.cmd(cmd % 'h1')
-    print '  h2 ', h2.cmd(cmd % 'h2')
+    # for receiver.
+    print '  %s  %s' % (str(receiver), receiver.cmd(cmd % str(receiver)))
+    # for each 2 hop host.
+    for host in hosts_2hop:
+        print '  %s  %s' % (str(host), host.cmd(cmd % str(host)))
+    # for each 6 hop host.
+    for host in hosts_6hop:
+        print '  %s  %s' % (str(host), host.cmd(cmd % str(host)))
 
-    print 'Setting queue size to %s...' % queue_size
+    # TODO(harshit): Set it for all switches agnostic of topography.
+    print 'Setting queue size to %s for all switches...' % queue_size
     cmd = ("tc qdisc change dev %s parent 1:1 "
-           "handle 10: netem limit %s" % ('s0-eth0', '16kb'))
+           "handle 10: netem limit %s" % ('s0-eth0', queue_size))
+    os.system(cmd)
+
+    print 'Starting flows...'
 
     # Start the receiver
     port = 5001
-    recvr.cmd('iperf -s -p', port,
-              '> %s/iperf_server.txt' % args.dir, '&')
+    receiver.cmd('iperf -s -p', port,
+                 '> %s/iperf_server.txt' % args.dir, '&')
 
-    waitListening(h1, recvr, port)
+    # Wait till the receiver server comes up by picking any 2 hop
+    # host as client.
+    waitListening(hosts_2hop[0], receiver, port)
 
-    for i in xrange(n_h1):
-        h1.cmd('iperf -Z bic -c %s -p %s -t %d -i 1 -yc > %s/iperf_%s.%d.txt &' % (
-            recvr.IP(), 5001, seconds, args.dir, 'h1', i))
-    for i in xrange(n_h2):
-        h2.cmd('iperf -Z bic -c %s -p %s -t %d -i 1 -yc > %s/iperf_%s.%d.txt &' % (
-            recvr.IP(), 5001, seconds, args.dir, 'h2', i))
+    # Start flows from 2 hop hosts.
+    for host in hosts_2hop:
+        for i in xrange(n_2hops):
+            host.cmd('iperf -Z bic -c %s -p %s -t %d -i 1 -yc >'
+                     ' %s/iperf_%s.%d.txt &' % (
+                    receiver.IP(), 5001, seconds, args.dir, str(host), i))
+    for host in hosts_6hop:
+        for i in xrange(n_6hops):
+            host.cmd('iperf -Z bic -c %s -p %s -t %d -i 1 -yc >'
+                     ' %s/iperf_%s.%d.txt &' % (
+                    receiver.IP(), 5001, seconds, args.dir, str(host), i))
 
     # TODO(bhelsley): intelligent wait to detect when client iperf processes
     # have exited.
     sleep(seconds + 5)
 
-    recvr.cmd('kill %iperf')
+    print 'Ending flows...'
+    receiver.cmd('kill %iperf')
 
     # Shut down monitors
     monitor.terminate()
@@ -195,10 +220,17 @@ def check_bandwidth(net, test_rate='100M'):
     for h2 in net.hosts[i+1:]:
       server_mbps, client_mbps = _GetIPerfResult(
           [h1, h2], l4Type='UDP', udpBw=test_rate)
-      result[('%s@%s' % (h1.name, h1.IP()), '%s@%s' % (h2.name, h2.IP()))] = (
+      result[('%s@%s' % (str(h1), h1.IP()), '%s@%s' % (str(h2), h2.IP()))] = (
           client_mbps, server_mbps)
 
   return result
+
+def run_single_switch_outcast(net):
+    recvr = net.getNodeByName('h0')
+    h1 = net.getNodeByName('h1')
+    h2 = net.getNodeByName('h2')
+    run_outcast(net, recvr, [h1], [h2], n_2hops=args.n1, n_6hops=args.n2,
+                rto_min=args.rto_min, queue_size=args.queue_size)
 
 
 def main():
@@ -213,7 +245,8 @@ def main():
     link = custom(TCLink, bw=args.bw, delay='1ms',
                   max_queue_size=200)
 
-    net = Mininet(topo=topo, host=host, link=link)
+    net = Mininet(topo=topo, host=host, link=link,
+                  controller=RemoteController)
 
     net.start()
 
@@ -229,8 +262,7 @@ def main():
       print pair, '=', result
 
     cprint("*** Running experiment", "magenta")
-    run_outcast(net, n_h1=args.n1, n_h2=args.n2, rto_min=args.rto_min,
-                queue_size=args.queue_size)
+    run_single_switch_outcast(net)
 
     net.stop()
     end = time()
