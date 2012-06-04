001b465b899454393a501aec3dcc1380fc2368c8
None <None>
2012-06-04 22:56:33 +0000

    Moving images to a level up

--- run_experiment.sh
+++ run_experiment.sh
@@ -23,18 +23,18 @@ run_single_experiment() {
 
     echo "Running experiment with $n1 2-hop flows and $n2 6-hop flows.\n"
     for i in `seq 1 $NUM_RUNS_PER_CONFIG`; do 
-	dir="$subdir/$i"
+	dir="$subdir/data_$i"
 	mn -c
 	python tcp_outcast.py --n1 $n1 --n2 $n2 --bw 100 -d $dir -t 20 \
 	    --ft=True --impatient=True
 	python generate_plots.py --tcpdump=$dir/tcp_dump.0_0_1-eth2.txt \
-	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $dir/result_500 \
+	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $subdir/result_500 \
 	    --bucket_size_ms=20 --end_time_ms=500 --start_time_ms=0
 	python generate_plots.py --tcpdump=$dir/tcp_dump.0_0_1-eth2.txt \
-	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $dir/result_5000 \
+	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $subdir/result_5000 \
 	    --bucket_size_ms=50 --end_time_ms=5000 --start_time_ms=0
 	python generate_plots.py --tcpdump=$dir/tcp_dump.0_0_1-eth2.txt \
-	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $dir/result_20000 \
+	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $subdir/result_20000 \
 	    --bucket_size_ms=200 --end_time_ms=20000 --start_time_ms=0
     done
 }
@@ -47,20 +47,20 @@ run_big_experiment() {
 
     echo "Running experiment with $n1 2-hop flows and $n2 6-hop flows.\n"
     for i in `seq 1 $NUM_RUNS_PER_CONFIG`; do 
-	dir="$subdir/$i"
+	dir="$subdir/data_$i"
 	mn -c
 	python tcp_outcast.py --n1 $n1 --n2 $n2 --bw 100 -d $dir -t 60 \
 	    --ft=True --impatient=True --iperf=/home/ubuntu/iperf-patched/src/iperf
 	python generate_plots.py --tcpdump=$dir/tcp_dump.0_0_1-eth2.txt \
-	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $dir/result_500 \
+	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $subdir/result_500 \
 	    --bucket_size_ms=20 --end_time_ms=5500 --start_time_ms=5000 \
 	    --skip_instant=True
 	python generate_plots.py --tcpdump=$dir/tcp_dump.0_0_1-eth2.txt \
-	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $dir/result_5000 \
+	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $subdir/result_5000 \
 	    --bucket_size_ms=50 --end_time_ms=10000 --start_time_ms=5000 \
 	    --skip_instant=True
 	python generate_plots.py --tcpdump=$dir/tcp_dump.0_0_1-eth2.txt \
-	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $dir/result_60000 \
+	    -r "10.0.0.2:5001" --outcast_host "10.0.0.3" -o $subdir/result_60000 \
 	    --bucket_size_ms=200 --end_time_ms=65000 --start_time_ms=5000 \
 	    --skip_instant=True
     done
